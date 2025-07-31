0f3c8385544c968d18103b7de0025bab0e2a2e20
23033s <23033@students.mrgs.school.nz>
2025-08-01 09:54:07 +1200

    trying to make a accordian on page 4

--- javascript.js
+++ javascript.js
@@ -20,3 +20,17 @@ dots[i].className = dots [i].className.replace(" active","");
 slides[slideIndex-1].style.display = "block";
 dots[slideIndex-1].className += " active";
 }
+script2 {
+  let acc = document.getElementsByClassName("accordion");
+  for (let i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          let panel = this.nextElementSibling;
+          if (panel.style.display === "block") {
+              panel.style.display = "none";
+          } else {
+              panel.style.display = "block";
+          }
+      });
+  }
+}
